0064bf7e8548b25617cbb9b6f979ec9d48bf0394
jkoh <jkoh@proofpoit.com>
2024-08-07 16:24:01 +0800

    Add row 3

--- chess/app.py
+++ chess/app.py
@@ -4,19 +4,23 @@ from chess_pipeline import run_pipeline
 import streamlit as st
 import altair as alt
 
-from app_helper import load_data, get_daily_win_loss
+from app_helper import load_data, get_daily_win_loss, wdl_by_color
 
 db = duckdb.connect(":memory:")
 db.sql("SET TimeZone='UTC'")
 st.set_page_config(layout="wide")
+
+domain = ['win', 'draw', 'lose']
+color_range = ['#2669c9', 'grey', '#f9abab']
 
 def row1(df):
     total_games= len(df)
     num_win_games = len(df[df['player_result'] == 'win'])
     win_perc = round(100 * num_win_games / total_games, 2)
     total_game_time = str(df['time_played'].sum())
+    highest_rating = df['player_rating'].max()
     
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4 = st.columns(4)
     
     col1.subheader('# Games Played')
     col1.subheader(total_games)
@@ -26,11 +30,11 @@ def row1(df):
     
     col3.subheader('Overall Win %')
     col3.subheader(f'{win_perc}%')
-
-def row2(df):
-    domain = ['win', 'draw', 'lose']
-    color_range = ['#2669c9', 'grey', '#f9abab']
     
+    col4.subheader('Highest Rating')
+    col4.subheader(highest_rating)
+
+def row2(df):   
     c1 = (alt.Chart(df)
           .mark_bar()
           .encode(
@@ -51,9 +55,57 @@ def row2(df):
     #           )
     #       )
 
-    st.header("Daily win/draw/lose results")
+    st.header("Daily Win/Draw/Loss results")
     
     st.altair_chart(c1, use_container_width=True)
+
+def row3(df):
+    col1, col2 = st.columns(2)
+    
+    bar1 = (alt.Chart(df)
+            .mark_bar()
+            .encode(
+                x=alt.X('sum(num_games)'),
+                y='player_color'
+            )
+    )
+    
+    text1 = (alt.Chart(df)
+            .mark_text(dx=-15, dy=3, color='black')
+            .encode(
+                x=alt.X('sum(num_games)'),
+                y='player_color',
+                text=alt.Text('sum(num_games)')
+            )
+    )
+    
+    bar2 = (alt.Chart(df)
+          .mark_bar()
+          .encode(
+                x=alt.X('sum(perc)').stack("normalize"),
+                # x='perc',
+                y='player_color',
+                color=alt.Color('result', scale=alt.Scale(domain=domain, range=color_range))
+                .legend(orient="bottom")
+            )
+    )
+    text2 = (alt.Chart(df)
+            .mark_text(dx=-15, dy=3, color='white')
+            .encode(
+                x=alt.X('sum(perc)').stack("normalize"),
+                # x='perc',
+                y='player_color',
+                detail='result',
+                text=alt.Text('perc', format='.1f')
+            )
+    )
+    
+    
+    col1.subheader('# Games Played by color')
+    col1.altair_chart(bar1+text1, use_container_width=True)
+    
+    col2.subheader('% Win/Draw/Loss by color')
+    col2.altair_chart(bar2+text2, use_container_width=True)
 
 st.sidebar.title('Welcome to Chess Dashboard!')
 prev_username = None
@@ -65,7 +117,7 @@ if username and ('username' not in st.session_state or username != st.session_st
     run_pipeline(db, username=username)
     df = load_data(db, username)
     
-    st.session_state.df = df
+    st.session_state.df = df # Save to session for filters
 
 if 'df' in st.session_state:
     df = st.session_state.df
@@ -87,4 +139,6 @@ if 'df' in st.session_state:
     df_daily_win_draw_lose = get_daily_win_loss(db, df_filtered)
     row2(df_daily_win_draw_lose)
 
-    # Row 3 - 
+    # Row 3 - Win by color
+    df_color_wdl = wdl_by_color(db, df_filtered)
+    row3(df_color_wdl)
--- chess/app_helper.py
+++ chess/app_helper.py
@@ -1,13 +1,17 @@
 def load_data(db, username):
-    df = db.sql(f"""
-    select
+    return db.sql(f"""
+        select
+        -- PLAYER DETAILS
         if(lower(white__username)='{username}', 'White', 'Black') as player_color
-        , case player_color
-            when 'White' then if(white__rating > black__rating, True, False)
-            else if(black__rating > white__rating, True, False)
-        end is_weaker_opponent
+        , if(player_color = 'White', white__rating, black__rating) as player_rating
         , if(player_color = 'White', white__result, black__result) as player_result
+        
+        -- OPPONENT DETAILS
+        , if(player_color = 'White', black__rating, white__rating) as opponent_rating
         , if(player_color = 'White', black__result, white__result) as opponent_result
+        
+        -- PLAYER-OPPONENT DETAILS
+        , player_rating > opponent_rating as is_weaker_opponent
         , case player_result
             -- WIN
             when 'win' then 'win'
@@ -26,8 +30,10 @@ def load_data(db, username):
             when 'resigned' then 'lose'
             when 'abandoned' then 'lose'
             when 'threecheck' then 'lose'
-        end as win_draw_lose
-        , if(player_result='win', opponent_result, player_result) as reason
+        end as player_wdl
+        , if(player_result='win', opponent_result, player_result) as player_wdl_reason
+        
+        -- GAME DETAILS
         , regexp_extract(pgn, '(ECO )"(.*)"', 2) as eco
         , cast(replace(regexp_extract(pgn, '(UTCDate )"(.*)"', 2), '.', '-') as date) as start_date
         , regexp_extract(pgn, '(StartTime )"(.*)"', 2) as start_time
@@ -37,18 +43,30 @@ def load_data(db, username):
         , epoch(time_played) as time_played_seconds
         , *
         from chess_data.player_games
-""").to_df()
-    return df
+    """).to_df()
 
 def get_daily_win_loss(db, df):
-    df = db.sql("""
+    return db.sql("""
         select 
         start_date as date
-        , win_draw_lose as result
+        , player_wdl as result
         , count(1) as num_games
         , sum(num_games) over(partition by start_date) as daily_num_games
         , 100.0 * num_games / daily_num_games as perc
         from df
-        group by start_date, win_draw_lose
+        group by start_date, player_wdl
     """).to_df()
-    return df
+
+def wdl_by_color(db, df):
+    return db.sql("""
+        select
+        coalesce(player_color, 'All') as player_color
+        , player_wdl as result
+        , count(1) as num_games
+        , round(100.0 * count(1) / sum(count(1)) over(partition by player_color),2) as perc
+        from df
+        group by grouping sets(
+            (player_color, player_wdl)
+            , (player_wdl)
+        )
+    """).to_df()
