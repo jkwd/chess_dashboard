e31a3e3088473a9176b7880b9164036dffff79ec
jkoh <jkoh@proofpoit.com>
2024-09-13 13:28:02 +0800

    Convert dev to using daemon and db to persist dagster info

--- chess_dagster/setup.py
+++ chess_dagster/setup.py
@@ -4,14 +4,16 @@ setup(
     name="chess",
     packages=find_packages(exclude=["chess_tests"]),
     install_requires=[
-        "duckdb==0.10.3",
         "dagster==1.8.0",
+        "dagster-webserver==1.8.0",
+        "dagster-postgres",
+        "dagster-docker",
         "dagster-embedded-elt==0.24.0",
         "dagster-duckdb-pandas==0.24.0",
         "dagster-duckdb==0.24.0",
+        "duckdb==0.10.3",
         "dlt[duckdb]==0.5.3",
         "python-chess==1.2.0",
-        "dagster-webserver==1.8.0",
     ],
     extras_require={"dev": ["pytest==8.3.2", "jupyter==1.0.0"]},
 )
