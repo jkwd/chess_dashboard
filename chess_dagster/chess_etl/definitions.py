5032193aebe85c004e3ae30af9bf18d82db0b63f
jkoh <jkoh@proofpoit.com>
2024-11-08 17:20:48 +0800

    Repoint definitions to dbt assets

--- chess_dagster/chess_etl/definitions.py
+++ chess_dagster/chess_etl/definitions.py
@@ -2,58 +2,41 @@ from dagster import (
     Definitions,
     ScheduleDefinition,
     define_asset_job,
-    load_assets_from_modules,
 )
-from chess_etl.assets import chess_source, asset_checks
-from chess_etl.assets.staging import stg_player_games
-from chess_etl.assets.prep import prep_player_games
-from chess_etl.assets.prep import prep_game_moves
-from chess_etl.assets.core import games
-from chess_etl.resources import dlt_resource, duckdb_resource
+from chess_etl.assets.dlt_assets import dagster_chess_assets
+from chess_etl.assets.dbt_assets import chess_dbt_dbt_assets
+# from chess_etl.assets.prep import prep_player_games
+# from chess_etl.assets.core import games
+from chess_etl.resources import dlt_resource, duckdb_resource, dbt_resource
+
 
 daily_refresh_schedule = ScheduleDefinition(
     job=define_asset_job(name="all_assets_job"), cron_schedule="0 0 * * *"
 )
 
 # Asset
-chess_source_assets = load_assets_from_modules([chess_source])
-stg_player_games_assets = load_assets_from_modules([stg_player_games])
-prep_player_games_assets = load_assets_from_modules([prep_player_games])
-prep_game_moves_assets = load_assets_from_modules([prep_game_moves])
-chess_core_assets = load_assets_from_modules([games])
+# dlt_chess_players_games_assets = load_assets_from_modules([dlt_chess_players_games])
+# prep_player_games_assets = load_assets_from_modules([prep_player_games])
+# chess_core_assets = load_assets_from_modules([games])
+# import_dbt_assets = load_assets_from_modules([dbt_assets])
 
 
 # Strict Asset Checks
 all_asset_checks = []
-for check_blob in stg_player_games.players_games_check_blobs:
-    all_asset_checks.append(asset_checks.make_check(check_blob))
 
-for check_blob in prep_player_games.prep_player_games_check_blobs:
-    all_asset_checks.append(asset_checks.make_check(check_blob))
+# for check_blob in prep_player_games.prep_player_games_check_blobs:
+#     all_asset_checks.append(asset_checks.make_check(check_blob))
 
-for check_blob in prep_game_moves.prep_game_moves_check_blobs:
-    all_asset_checks.append(asset_checks.make_check(check_blob))
-
-for check_blob in games.games_check_blobs:
-    all_asset_checks.append(asset_checks.make_check(check_blob))
-
-
-# Approx Asset Checks
-for check_blob in prep_game_moves.game_moves_approx_check_blobs:
-    all_asset_checks.append(asset_checks.make_perc_approx_check(check_blob))
-
+# for check_blob in games.games_check_blobs:
+#     all_asset_checks.append(asset_checks.make_check(check_blob))
 
 # Must be last
 defs = Definitions(
-    assets=[*chess_source_assets,
-            *stg_player_games_assets,
-            *prep_player_games_assets,
-            *prep_game_moves_assets,
-            *chess_core_assets],
-    asset_checks=all_asset_checks,
+    assets=[dagster_chess_assets, chess_dbt_dbt_assets],
     resources={
         "dlt": dlt_resource,
         "duckdb": duckdb_resource,
+        "dbt": dbt_resource,
     },
     schedules=[daily_refresh_schedule],
 )
